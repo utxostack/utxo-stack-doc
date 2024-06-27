e63d7cd4fdad2efcc779658bb3f3154374494b91
Flouse <1297478+Flouse@users.noreply.github.com>
2024-06-27 02:11:44 +0000

    config: Update baseUrl to '/' for Vercel

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -17,7 +17,7 @@ const config = {
   url: 'https://docs.utxostack.network',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/utxo-stack-doc/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
