419d7bb1aff3b5c382a72893b4587fb7b1c6d1f8
INS <j893412899@outlook.com>
2024-07-05 16:12:06 +0800

    feat: light theme (#30)

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -78,11 +78,6 @@ const config = {
     ({
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
-      colorMode: {
-        defaultMode: 'dark',
-        disableSwitch: true,
-        respectPrefersColorScheme: false,
-      },
       navbar: {
         title: 'UTXO Stack',
         logo: {
